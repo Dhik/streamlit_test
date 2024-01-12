0d8ec3e5a61a3cc7e6a88213d8009daf8b59edc6
dhik <mohammaddhikri@gmail.com>
2024-01-12 15:29:11 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.set_page_config(
+    page_title='HEART FAILURE-EDA',
+    layout = 'wide',
+    initial_sidebar_state='expanded'
+)
+st.title("Hello World")
