77bcfd8a03c75d7c52b99443e4043064c999efac
dhik <mohammaddhikri@gmail.com>
2024-01-30 19:39:36 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
-import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+import streamlit as st 
+from Home import face_rec
+from streamlit_webrtc import webrtc_streamer
+import av
+import time
 
 st.set_page_config(
     page_title='HEART FAILURE-EDA',
